f9e9613e532383c2b8c5ccd0476364d15ab0471f
Pančić Aleksa <35733741+AleksaPancic@users.noreply.github.com>
2023-01-10 19:36:18 +0100

    Update Cart.java

--- src/main/java/app/models/dto/Cart.java
+++ src/main/java/app/models/dto/Cart.java
@@ -3,7 +3,7 @@ package app.models.dto;
 public class Cart {
     private final Integer id;
     private final Integer quantity;
-
+//idk
     public Cart(Integer id, Integer quantity) {
         this.id = id;
         this.quantity = quantity;
